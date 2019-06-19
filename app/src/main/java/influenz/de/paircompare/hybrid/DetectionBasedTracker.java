e19a26544b2d97815b3a5bfedd37317c507d5ee4
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:58:49 +0200

    Update DetectionBasedTracker.java

--- app/src/main/java/influenz/de/paircompare/hybrid/DetectionBasedTracker.java
+++ app/src/main/java/influenz/de/paircompare/hybrid/DetectionBasedTracker.java
@@ -4,45 +4,38 @@ package influenz.de.paircompare.hybrid;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 
-public class DetectionBasedTracker
-{
-    public DetectionBasedTracker(final String cascadeName, final int minFaceSize)
-    {
-        nativeObj = nativeCreateObject(cascadeName, minFaceSize);
-    }
+public class DetectionBasedTracker {
+ public DetectionBasedTracker(final String cascadeName, final int minFaceSize) {
+  nativeObj = nativeCreateObject(cascadeName, minFaceSize);
+ }
 
-    public void start()
-    {
-        nativeStart(nativeObj);
-    }
+ public void start() {
+  nativeStart(nativeObj);
+ }
 
-    public void stop()
-    {
-        nativeStop(nativeObj);
-    }
+ public void stop() {
+  nativeStop(nativeObj);
+ }
 
-    public void detect(Mat imageGray, MatOfRect faces)
-    {
-        nativeDetect(nativeObj, imageGray.getNativeObjAddr(), faces.getNativeObjAddr());
-    }
+ public void detect(Mat imageGray, MatOfRect faces) {
+  nativeDetect(nativeObj, imageGray.getNativeObjAddr(), faces.getNativeObjAddr());
+ }
 
-    public void release()
-    {
-        nativeDestroyObject(nativeObj);
-        nativeObj = 0;
-    }
+ public void release() {
+  nativeDestroyObject(nativeObj);
+  nativeObj = 0;
+ }
 
-    public void setFaceSize(final int faceSize )
-    {
-        nativeSetFaceSize(nativeObj, faceSize);
-    }
+ public void setFaceSize(final int faceSize) {
+  nativeSetFaceSize(nativeObj, faceSize);
+ }
 
-    private long nativeObj = 0;
+ private long nativeObj = 0;
 
-    private static native long nativeCreateObject(final String cascadeName, final int minFaceSize);
-    private static native void nativeDestroyObject(final long thiz);
-    private static native void nativeStart(final long thiz);
-    private static native void nativeStop(final long thiz);
-    private static native void nativeSetFaceSize(final long thiz, final int size);
-    private static native void nativeDetect(final long thiz, final long inputImage, final long faces);
+ private static native long nativeCreateObject(final String cascadeName, final int minFaceSize);
+ private static native void nativeDestroyObject(final long thiz);
+ private static native void nativeStart(final long thiz);
+ private static native void nativeStop(final long thiz);
+ private static native void nativeSetFaceSize(final long thiz, final int size);
+ private static native void nativeDetect(final long thiz, final long inputImage, final long faces);
 }
