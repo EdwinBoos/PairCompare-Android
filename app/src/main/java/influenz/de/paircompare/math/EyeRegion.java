d5d485a3eedc41fe406b14ac661f18787391c559
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 11:01:43 +0200

    Update EyeRegion.java

--- app/src/main/java/influenz/de/paircompare/math/EyeRegion.java
+++ app/src/main/java/influenz/de/paircompare/math/EyeRegion.java
@@ -1,39 +1,31 @@
 package influenz.de.paircompare.math;
 import org.opencv.core.Rect;
 
-public final class EyeRegion
-{
+public final class EyeRegion {
 
-    private final Rect face;
+ private final Rect face;
 
-    public EyeRegion(final Rect face)
-    {
-        this.face = face;
-    }
+ public EyeRegion(final Rect face) {
+  this.face = face;
+ }
 
-    public Rect computeLeftEyeRegion()
-    {
-       return new Rect(
-               face.x + face.width / 16 + (face.width - 2 * face.width / 16) / 2,
-               (int) (face.y + (face.height / 4.5)),
-               (face.width - 2 * face.width / 16) / 2,
-               (int) (face.height / 3.0)
-       );
-    }
+ public Rect computeLeftEyeRegion() {
+  return new Rect(
+   face.x + face.width / 16 + (face.width - 2 * face.width / 16) / 2,
+   (int)(face.y + (face.height / 4.5)),
+   (face.width - 2 * face.width / 16) / 2,
+   (int)(face.height / 3.0)
+  );
+ }
 
 
-    public Rect computeRightEyeRegion()
-    {
-        return new Rect(
-                face.x + face.width / 16,
-                (int) (face.y + (face.height / 4.5)),
-                (face.width - 2 * face.width / 16) / 2,
-                (int) (face.height / 3.0)
-        );
-    }
-
-
-
-
+ public Rect computeRightEyeRegion() {
+  return new Rect(
+   face.x + face.width / 16,
+   (int)(face.y + (face.height / 4.5)),
+   (face.width - 2 * face.width / 16) / 2,
+   (int)(face.height / 3.0)
+  );
+ }
 
 }
