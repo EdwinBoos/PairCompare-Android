e461d4b70d03c010273c461050b19b50135076ae
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:41:02 +0200

    Update NullObjectConverter.java

--- app/src/main/java/influenz/de/paircompare/converter/NullObjectConverter.java
+++ app/src/main/java/influenz/de/paircompare/converter/NullObjectConverter.java
@@ -7,14 +7,12 @@ import org.opencv.core.Mat;
 
 import influenz.de.paircompare.interfaces.IConverter;
 
-public final class NullObjectConverter implements IConverter
-{
-    @Override
-    public Object convert(final Mat mat, final Bitmap bitmap)
-    {
-        Log.e("NullObjectConverter", "Please check the action you provided to the converterFactory, " +
-                "something went wrong.");
+public final class NullObjectConverter implements IConverter {
+ @Override
+ public Object convert(final Mat mat, final Bitmap bitmap) {
+  Log.e("NullObjectConverter", "Please check the action you provided to the converterFactory, " +
+   "something went wrong.");
 
-        return this;
-    }
+  return this;
+ }
 }
