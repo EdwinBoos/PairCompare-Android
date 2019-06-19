bff309487ce96f89bbb4c6b24392d4a6f9abc41d
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:41:27 +0200

    Update Chin.java

--- app/src/main/java/influenz/de/paircompare/facefeature/Chin.java
+++ app/src/main/java/influenz/de/paircompare/facefeature/Chin.java
@@ -4,24 +4,20 @@ import android.graphics.Point;
 import java.util.ArrayList;
 import influenz.de.paircompare.math.Angle;
 
-public class Chin
-{
+public class Chin {
 
-    private final ArrayList<Point> chinLandmarks;
+ private final ArrayList < Point > chinLandmarks;
 
-    public Chin(final ArrayList<Point> chinLandmarks)
-    {
-        this.chinLandmarks = chinLandmarks;
-    }
+ public Chin(final ArrayList < Point > chinLandmarks) {
+  this.chinLandmarks = chinLandmarks;
+ }
 
-    public double getAngle()
-    {
-        return new Angle(chinLandmarks.get(1), chinLandmarks.get(2), chinLandmarks.get(3)).compute();
-    }
+ public double getAngle() {
+  return new Angle(chinLandmarks.get(1), chinLandmarks.get(2), chinLandmarks.get(3)).compute();
+ }
 
-    public double getAngleInDegrees()
-    {
-       return Math.toDegrees(new Angle(chinLandmarks.get(1), chinLandmarks.get(2), chinLandmarks.get(3)).compute());
-    }
+ public double getAngleInDegrees() {
+  return Math.toDegrees(new Angle(chinLandmarks.get(1), chinLandmarks.get(2), chinLandmarks.get(3)).compute());
+ }
 
 }
