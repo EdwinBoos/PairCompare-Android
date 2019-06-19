c9c6b9ffe0dbb2d72c192f8166fbd6fb0a452bb2
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:40:07 +0200

    beautify

--- app/src/main/java/influenz/de/paircompare/converter/Bitmap2MatConverter.java
+++ app/src/main/java/influenz/de/paircompare/converter/Bitmap2MatConverter.java
@@ -6,14 +6,12 @@ import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import influenz.de.paircompare.interfaces.IConverter;
 
-public final class Bitmap2MatConverter implements IConverter
-{
+public final class Bitmap2MatConverter implements IConverter {
 
-    @Override
-    public Object convert(final Mat mat, final Bitmap bitmap)
-    {
-        Utils.bitmapToMat(bitmap, mat);
-        return mat;
-    }
+ @Override
+ public Object convert(final Mat mat, final Bitmap bitmap) {
+  Utils.bitmapToMat(bitmap, mat);
+  return mat;
+ }
 
 }
