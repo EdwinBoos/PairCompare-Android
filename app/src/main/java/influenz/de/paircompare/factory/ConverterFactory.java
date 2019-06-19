3a2a43bce61134e7965ed7d5dbf22892e32f5f67
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:47:14 +0200

    Update ConverterFactory.java

--- app/src/main/java/influenz/de/paircompare/factory/ConverterFactory.java
+++ app/src/main/java/influenz/de/paircompare/factory/ConverterFactory.java
@@ -4,23 +4,17 @@ import influenz.de.paircompare.converter.Bitmap2MatConverter;
 import influenz.de.paircompare.converter.Mat2BitmapConverter;
 import influenz.de.paircompare.converter.NullObjectConverter;
 
-public final class ConverterFactory
-{
+public final class ConverterFactory {
 
-    public static final String BITMAP_2_MAT_ACTION = "bitmap2mat";
-    public static final String MAT_2_BITMAP_ACTION = "mat2Bitmap";
+ public static final String BITMAP_2_MAT_ACTION = "bitmap2mat";
+ public static final String MAT_2_BITMAP_ACTION = "mat2Bitmap";
 
-    public IConverter build(final String action)
-    {
-        if(action.equalsIgnoreCase(this.BITMAP_2_MAT_ACTION))
-        {
-            return new Bitmap2MatConverter();
-        }
-        else if(action.equalsIgnoreCase(this.MAT_2_BITMAP_ACTION))
-        {
-            return new Mat2BitmapConverter();
-        }
-        else return new NullObjectConverter();
-    }
+ public IConverter build(final String action) {
+  if (action.equalsIgnoreCase(this.BITMAP_2_MAT_ACTION)) {
+   return new Bitmap2MatConverter();
+  } else if (action.equalsIgnoreCase(this.MAT_2_BITMAP_ACTION)) {
+   return new Mat2BitmapConverter();
+  } else return new NullObjectConverter();
+ }
 
 }
