356ba531c28d9b814fc75820efcf81d3a0ea7798
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:55:11 +0200

    Update FacialLandmarksFragment.java

--- app/src/main/java/influenz/de/paircompare/fragment/FacialLandmarksFragment.java
+++ app/src/main/java/influenz/de/paircompare/fragment/FacialLandmarksFragment.java
@@ -16,42 +16,39 @@ import influenz.de.paircompare.interfaces.IEnum;
 import influenz.de.paircompare.observer.ChildFragmentObservable;
 import influenz.de.paircompare.util.LandmarksCanvas;
 
-public class FacialLandmarksFragment extends Fragment implements Observer
-{
+public class FacialLandmarksFragment extends Fragment implements Observer {
 
-    private ImageView imageViewFace1;
-    private ImageView imageViewFace2;
+ private ImageView imageViewFace1;
+ private ImageView imageViewFace2;
 
-    @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState)
-    {
-        final View view = inflater.inflate(R.layout.fragment_facial_landmarks, container, false);
-        imageViewFace1 = (ImageView) view.findViewById(R.id.face_1_id);
-        imageViewFace2 = (ImageView) view.findViewById(R.id.face_2_id);
-        return view;
-    }
+ @Override
+ public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+  final View view = inflater.inflate(R.layout.fragment_facial_landmarks, container, false);
+  imageViewFace1 = (ImageView) view.findViewById(R.id.face_1_id);
+  imageViewFace2 = (ImageView) view.findViewById(R.id.face_2_id);
+  return view;
+ }
 
-    @Override
-    public void update(final Observable observable, final Object argument)
-    {
+ @Override
+ public void update(final Observable observable, final Object argument) {
 
-        final ChildFragmentObservable childFragmentObservable = (ChildFragmentObservable) observable;
-        final ArrayList<Point> face1Landmarks = childFragmentObservable.getFace1Landmarks();
-        final ArrayList<Point> face2Landmarks = childFragmentObservable.getFace2Landmarks();
-        final Bitmap bitmapFace1 = childFragmentObservable.getBitmapFace1();
-        final Bitmap bitmapFace2 = childFragmentObservable.getBitmapFace2();
-        final Paint faceLandmarkPaint = new Paint();
-        final LandmarksCanvas canvasFace1 = new LandmarksCanvas(bitmapFace1);
-        final LandmarksCanvas canvasFace2 = new LandmarksCanvas(bitmapFace2);
+  final ChildFragmentObservable childFragmentObservable = (ChildFragmentObservable) observable;
+  final ArrayList < Point > face1Landmarks = childFragmentObservable.getFace1Landmarks();
+  final ArrayList < Point > face2Landmarks = childFragmentObservable.getFace2Landmarks();
+  final Bitmap bitmapFace1 = childFragmentObservable.getBitmapFace1();
+  final Bitmap bitmapFace2 = childFragmentObservable.getBitmapFace2();
+  final Paint faceLandmarkPaint = new Paint();
+  final LandmarksCanvas canvasFace1 = new LandmarksCanvas(bitmapFace1);
+  final LandmarksCanvas canvasFace2 = new LandmarksCanvas(bitmapFace2);
 
-        faceLandmarkPaint.setColor(getContext().getResources().getColor(R.color.colorAccent));
-        faceLandmarkPaint.setStrokeWidth(IEnum.ThicknessEnum.strokeWidth);
-        faceLandmarkPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        canvasFace1.drawLandmarksAsCircle(face1Landmarks, IEnum.RadiusEnum.canvasRadius, faceLandmarkPaint );
-        canvasFace2.drawLandmarksAsCircle(face2Landmarks, IEnum.RadiusEnum.canvasRadius, faceLandmarkPaint );
+  faceLandmarkPaint.setColor(getContext().getResources().getColor(R.color.colorAccent));
+  faceLandmarkPaint.setStrokeWidth(IEnum.ThicknessEnum.strokeWidth);
+  faceLandmarkPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+  canvasFace1.drawLandmarksAsCircle(face1Landmarks, IEnum.RadiusEnum.canvasRadius, faceLandmarkPaint);
+  canvasFace2.drawLandmarksAsCircle(face2Landmarks, IEnum.RadiusEnum.canvasRadius, faceLandmarkPaint);
 
-        imageViewFace1.setImageBitmap(bitmapFace1);
-        imageViewFace2.setImageBitmap(bitmapFace2);
+  imageViewFace1.setImageBitmap(bitmapFace1);
+  imageViewFace2.setImageBitmap(bitmapFace2);
 
-    }
+ }
 }
