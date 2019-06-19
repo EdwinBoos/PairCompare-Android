90184a841e8d5707ed0465eb2e70dfcb105c2110
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 11:03:22 +0200

    Update ChildFragmentObservable.java

--- app/src/main/java/influenz/de/paircompare/observer/ChildFragmentObservable.java
+++ app/src/main/java/influenz/de/paircompare/observer/ChildFragmentObservable.java
@@ -5,51 +5,45 @@ import android.graphics.Point;
 import java.util.ArrayList;
 import java.util.Observable;
 
-public final class ChildFragmentObservable extends Observable
-{
+public final class ChildFragmentObservable extends Observable {
 
-    private final Bitmap bitmapFace1;
-    private final Bitmap bitmapFace2;
-    private final ArrayList<Point> face1Landmarks;
-    private final ArrayList<Point> face2Landmarks;
+ private final Bitmap bitmapFace1;
+ private final Bitmap bitmapFace2;
+ private final ArrayList < Point > face1Landmarks;
+ private final ArrayList < Point > face2Landmarks;
 
-    public ChildFragmentObservable(final ArrayList<Point> face1Landmarks,
-                                   final ArrayList<Point> face2Landmarks,
-                                   final Bitmap bitmapFace1,
-                                   final Bitmap bitmapFace2) {
+ public ChildFragmentObservable(final ArrayList < Point > face1Landmarks,
+  final ArrayList < Point > face2Landmarks,
+  final Bitmap bitmapFace1,
+  final Bitmap bitmapFace2) {
 
-        this.bitmapFace1 = bitmapFace1;
-        this.bitmapFace2 = bitmapFace2;
-        this.face1Landmarks = face1Landmarks;
-        this.face2Landmarks = face2Landmarks;
-    }
+  this.bitmapFace1 = bitmapFace1;
+  this.bitmapFace2 = bitmapFace2;
+  this.face1Landmarks = face1Landmarks;
+  this.face2Landmarks = face2Landmarks;
+ }
 
-    @Override
-    public void notifyObservers()
-    {
-        setChanged();
-        super.notifyObservers();
-    }
+ @Override
+ public void notifyObservers() {
+  setChanged();
+  super.notifyObservers();
+ }
 
-    public ArrayList<Point> getFace1Landmarks()
-    {
-        return face1Landmarks;
-    }
+ public ArrayList < Point > getFace1Landmarks() {
+  return face1Landmarks;
+ }
 
-    public ArrayList<Point> getFace2Landmarks()
-    {
-        return face2Landmarks;
-    }
+ public ArrayList < Point > getFace2Landmarks() {
+  return face2Landmarks;
+ }
 
-    public Bitmap getBitmapFace1()
-    {
-        return bitmapFace1;
-    }
+ public Bitmap getBitmapFace1() {
+  return bitmapFace1;
+ }
 
-    public Bitmap getBitmapFace2()
-    {
-        return bitmapFace2;
-    }
+ public Bitmap getBitmapFace2() {
+  return bitmapFace2;
+ }
 
 
 }
