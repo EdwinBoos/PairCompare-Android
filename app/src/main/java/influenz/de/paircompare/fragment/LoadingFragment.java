687a2a2c368db4ee27c1f0ac9e2535e489e60a4c
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 10:55:46 +0200

    Update LoadingFragment.java

--- app/src/main/java/influenz/de/paircompare/fragment/LoadingFragment.java
+++ app/src/main/java/influenz/de/paircompare/fragment/LoadingFragment.java
@@ -25,94 +25,87 @@ import influenz.de.paircompare.observer.BitmapsObservable;
 import influenz.de.paircompare.observer.ChildFragmentObservable;
 import influenz.de.paircompare.util.XMLFileLoader;
 
-public class LoadingFragment extends Fragment implements Observer, IEnum
-{
+public class LoadingFragment extends Fragment implements Observer, IEnum {
 
-    private Bitmap bitmapFace1;
-    private Bitmap bitmapFace2;
-    private FacialLandmarksFragment facialLandmarksFragment;
-    private ComputingFragment computingFragment;
-    private ViewPager viewPager;
-    private PageAdapter pageAdapter;
-    private ProgressBar progressBarView;
-    private ArrayList<Point> face1Landmarks;
-    private ArrayList<Point> face2Landmarks;
-
-
-    @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState)
-    {
-        final View view =  inflater.inflate(R.layout.fragment_loading, container, false);
-
-        progressBarView = (ProgressBar) view.findViewById(R.id.spin_kit_id);
-        viewPager = (ViewPager) view.findViewById(R.id.view_pager_id);
-
-        pageAdapter = new PageAdapter(getFragmentManager());
-        viewPager.setPageTransformer(false, new CubeOutTransformer());
-
-        facialLandmarksFragment = new FacialLandmarksFragment();
-        computingFragment = new ComputingFragment();
-
-        IFragmentCreatedListener callback = (IFragmentCreatedListener) getActivity();
-        callback.onFragmentReady();
-
-        return view;
-    }
+ private Bitmap bitmapFace1;
+ private Bitmap bitmapFace2;
+ private FacialLandmarksFragment facialLandmarksFragment;
+ private ComputingFragment computingFragment;
+ private ViewPager viewPager;
+ private PageAdapter pageAdapter;
+ private ProgressBar progressBarView;
+ private ArrayList < Point > face1Landmarks;
+ private ArrayList < Point > face2Landmarks;
 
 
-    @SuppressLint("StaticFieldLeak")
-    @Override
-    public void update(final Observable observable, final Object argument)
-    {
-        final BitmapsObservable bitmapsObservable = (BitmapsObservable) observable;
-        bitmapFace1 = bitmapsObservable.getBitmapArrayList().get(0);
-        bitmapFace2 = bitmapsObservable.getBitmapArrayList().get(1);
+ @Override
+ public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+  final View view = inflater.inflate(R.layout.fragment_loading, container, false);
 
-        new AsyncTask<Void, Void, Void>()
-        {
+  progressBarView = (ProgressBar) view.findViewById(R.id.spin_kit_id);
+  viewPager = (ViewPager) view.findViewById(R.id.view_pager_id);
 
-            @Override
-            protected void onPreExecute()
-            {
-                progressBarView.setVisibility(View.VISIBLE);
-            }
+  pageAdapter = new PageAdapter(getFragmentManager());
+  viewPager.setPageTransformer(false, new CubeOutTransformer());
 
-            @Override
-            protected Void doInBackground(final Void... params)
-            {
+  facialLandmarksFragment = new FacialLandmarksFragment();
+  computingFragment = new ComputingFragment();
 
-                final File rawFile = new XMLFileLoader(getContext(), R.raw.shape_predictor_68_face_landmarks).load();
-                face1Landmarks =
-                        new FaceDet(rawFile.getAbsolutePath())
-                                .detect(bitmapFace1)
-                                .get(FaceEnum.Face1Index)
-                                .getFaceLandmarks();
+  IFragmentCreatedListener callback = (IFragmentCreatedListener) getActivity();
+  callback.onFragmentReady();
 
-                face2Landmarks =
-                        new FaceDet(rawFile.getAbsolutePath())
-                                .detect(bitmapFace2)
-                                .get(FaceEnum.Face2Index)
-                                .getFaceLandmarks();
+  return view;
+ }
 
-                return null;
-            }
 
-            @Override
-            protected void onPostExecute(final Void unused)
-            {
-                progressBarView.setVisibility(View.GONE);
+ @SuppressLint("StaticFieldLeak")
+ @Override
+ public void update(final Observable observable, final Object argument) {
+  final BitmapsObservable bitmapsObservable = (BitmapsObservable) observable;
+  bitmapFace1 = bitmapsObservable.getBitmapArrayList().get(0);
+  bitmapFace2 = bitmapsObservable.getBitmapArrayList().get(1);
 
-                pageAdapter.addFragment(facialLandmarksFragment);
-                pageAdapter.addFragment(computingFragment);
-                viewPager.setAdapter(pageAdapter);
+  new AsyncTask < Void, Void, Void > () {
 
-                final ChildFragmentObservable childFragmentObservable = new ChildFragmentObservable(face1Landmarks, face2Landmarks, bitmapFace1, bitmapFace2);
-                childFragmentObservable.addObserver(facialLandmarksFragment);
-                childFragmentObservable.addObserver(computingFragment);
-                childFragmentObservable.notifyObservers();
+   @Override
+   protected void onPreExecute() {
+    progressBarView.setVisibility(View.VISIBLE);
+   }
 
-            }
+   @Override
+   protected Void doInBackground(final Void...params) {
 
-        }.execute();
-    }
+    final File rawFile = new XMLFileLoader(getContext(), R.raw.shape_predictor_68_face_landmarks).load();
+    face1Landmarks =
+     new FaceDet(rawFile.getAbsolutePath())
+     .detect(bitmapFace1)
+     .get(FaceEnum.Face1Index)
+     .getFaceLandmarks();
+
+    face2Landmarks =
+     new FaceDet(rawFile.getAbsolutePath())
+     .detect(bitmapFace2)
+     .get(FaceEnum.Face2Index)
+     .getFaceLandmarks();
+
+    return null;
+   }
+
+   @Override
+   protected void onPostExecute(final Void unused) {
+    progressBarView.setVisibility(View.GONE);
+
+    pageAdapter.addFragment(facialLandmarksFragment);
+    pageAdapter.addFragment(computingFragment);
+    viewPager.setAdapter(pageAdapter);
+
+    final ChildFragmentObservable childFragmentObservable = new ChildFragmentObservable(face1Landmarks, face2Landmarks, bitmapFace1, bitmapFace2);
+    childFragmentObservable.addObserver(facialLandmarksFragment);
+    childFragmentObservable.addObserver(computingFragment);
+    childFragmentObservable.notifyObservers();
+
+   }
+
+  }.execute();
+ }
 }
