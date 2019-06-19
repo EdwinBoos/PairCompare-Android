828232e7b9283593ec201970c3b4cd400f36b469
INFLUENZ <edwin.boos@gmx.de>
2019-06-19 11:02:09 +0200

    Update FaceRegion.java

--- app/src/main/java/influenz/de/paircompare/math/FaceRegion.java
+++ app/src/main/java/influenz/de/paircompare/math/FaceRegion.java
@@ -2,23 +2,20 @@ package influenz.de.paircompare.math;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 
-public final class FaceRegion
-{
+public final class FaceRegion {
 
-    private final Rect face;
+ private final Rect face;
 
-    public FaceRegion(final Rect face)
-    {
-        this.face = face;
-    }
+ public FaceRegion(final Rect face) {
+  this.face = face;
+ }
 
-    public Point computeCenterPoint()
-    {
+ public Point computeCenterPoint() {
 
-       return new Point((face.x + face.width + face.x) / 2,
-                                 (face.y + face.y + face.height) / 2);
+  return new Point((face.x + face.width + face.x) / 2,
+   (face.y + face.y + face.height) / 2);
 
-    }
+ }
 
 
 }
